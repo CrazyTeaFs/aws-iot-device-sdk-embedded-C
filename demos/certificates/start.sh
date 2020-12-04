1229f6550400f2a69199fe064de1b23bca05f867
JiaHuaGuo <xiaomomo2012@hotmail.com>
2020-12-04 08:41:15 +0000

    Add files via upload

---
+++ demos/certificates/start.sh
@@ -0,0 +1,18 @@
+# stop script on error
+set -e
+
+# Check to see if root CA file exists, download if not
+if [ ! -f ./root-CA.crt ]; then
+  printf "\nDownloading AWS IoT Root CA certificate from AWS...\n"
+  curl https://www.amazontrust.com/repository/AmazonRootCA1.pem > root-CA.crt
+fi
+
+# install AWS Device SDK for NodeJS if not already installed
+if ! node -e "require('aws-iot-device-sdk')" &> /dev/null; then
+  printf "\nInstalling AWS SDK...\n"
+  npm install aws-iot-device-sdk
+fi
+
+# run pub/sub sample app using certificates downloaded in package
+printf "\nRunning pub/sub sample application...\n"
+node node_modules/aws-iot-device-sdk/examples/device-example.js --host-name=a300cq358hzj92-ats.iot.us-east-1.amazonaws.com --private-key=demo.private.key --client-certificate=demo.cert.pem --ca-certificate=root-CA.crt --client-id=sdk-nodejs-8956b46f-ee60-4b20-8770-debcf3cc6871
